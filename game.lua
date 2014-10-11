dcf2de3ea4421b080f143e56b3f5f6bc31d6ef7f
rvergis <ron.vergis@gmail.com>
2014-10-11 07:06:39 -0700

    RC1

--- game.lua
+++ game.lua
@@ -13,55 +13,191 @@
 ---------------------------------------------------------------------------------------
 
 require "main"
-require "tinkeracademy"
 
-ball = nil
+uuid = require("uuid")
 
+user_uid = uuid.new()
+
+NEXT_TURN_URL = "http://192.168.1.5/SimulPlay/nextturn"
+
+ADD_SCORE_URL = "http://192.168.1.5/SimulPlay/addscore"
+
+RESET_SCORE_URL = "http://192.168.1.5/SimulPlay/resetscore"
+
+myTurnCallback = nil
+
+local physics = require("physics")
+
+local json = require("json")
+
+local ball = nil
+
+-------------------------------------------------------------------------------
+-- Game Logic
+-------------------------------------------------------------------------------
 function onBallTapped(event)
-	addScore()
+	kickBall()
+end
+
+function onGameUpdate(score, shouldDropBall)
+	displayScore(score)
+	if shouldDropBall then
+		dropBall()	
+	end
+end
+
+function startGame()
+	initializeGame(onGameUpdate)
+end
+
+-------------------------------------------------------------------------------
+-- Game API
+-------------------------------------------------------------------------------
+function initializeGame(callback, debug)
+	debugMode = debug or false
+	myTurnCallback = callback
+	startPlayTimer()
+	listenToCollisionEvents()
+end
+
+function kickBall()
 	math.randomseed(os.time())
 	local randomXImpulse = math.random(500, 1000)
 	local randomYImpulse = math.random(-1000, -500)
 	ball:applyLinearImpulse(randomXImpulse, randomYImpulse, ball.x, ball.y)
 	ball:applyAngularImpulse(100)
+	addScore()
 end
 
-function myScore(score) 
-	displayScore(score)
+function dropBall()
+	createBall()
 end
 
-function myTurn(score)
-	displayScore(score)
+-------------------------------------------------------------------------------
+-- Helper Functions
+-------------------------------------------------------------------------------
+
+function createBall()
 	if (ball ~= nil) then
 		ball:removeSelf()
 	end
-	ball = display.newImage( "soccer_ball.png", 180, -50 )
 	math.randomseed(os.time())
-	ball.x = math.random(60, 240)	
-	ball.rotation = 5
-	ball:addEventListener("tap", onBallTapped)
+	local xpos = 0
+	local ypos = 0
+	local linearXImpulse = 0
+	local linearYImpulse = 0
+	local location = math.random(1,3)
+	if (location == 1) then
+		ball = display.newImage( "soccer_ball.png", -50, 60 )
+		linearXImpulse = math.random(200, 400)
+		linearYImpulse = math.random(-200, -100)
+	elseif (location == 2) then
+		ball = display.newImage( "soccer_ball.png", 370, 60 )
+		linearXImpulse = math.random(-400, -200)
+		linearYImpulse = math.random(-200, -100)
+	else
+		ball = display.newImage( "soccer_ball.png", 180, -50 )
+		linearXImpulse = math.random(-200, 200)
+		linearYImpulse = math.random(-200, -100)
+	end
+	local dampen = 4
 	physics.addBody( ball, { density=3.0, friction=0.5, bounce=0.8 } )
+	ball:applyLinearImpulse(linearXImpulse/dampen, linearYImpulsedampen, ball.x, ball.y)
+	ball:applyAngularImpulse(100)
+	ball:addEventListener("tap", onBallTapped)
 end
 
-function startGame()
-	initializeGame()
-	registerMyTurn(myTurn)
-	registerMyScore(myScore)
-	listenToCollisionEvents()
+function addScore()
+	logText("GREAT!!!")
+	local url_params = "?user_uid="..user_uid
+	local url = ADD_SCORE_URL..url_params
+	network.request( url, "GET", networkListener)
 end
 
+function resetScore()
+	logText("OOPS!")
+	local url_params = "?user_uid="..user_uid
+	local url = RESET_SCORE_URL..url_params
+	network.request( url, "GET", networkListener)
+end
+
+-------------------------------------------------------------------------------
+-- Network listener
+-------------------------------------------------------------------------------
+function networkListener(event)
+	if (event.phase == "ended") then
+		local decoded, pos, msg = json.decode(event.response)
+		if decoded then
+			server_user_uid = decoded.user_uid
+			server_score = decoded.score
+			if (server_user_uid == user_uid) then
+				debugText("YOUR TURN!")
+				myTurnCallback(server_score, true)
+			else
+				debugText("OTHERS TURN!")
+				myTurnCallback(server_score, false)
+			end
+		end
+	end
+end
+
+-------------------------------------------------------------------------------
+-- Game Timer
+-------------------------------------------------------------------------------
+function updateTimer()
+	local url_params = "?user_uid="..user_uid
+	local url = NEXT_TURN_URL..url_params
+	network.request( url, "GET", networkListener)
+end
+
+function startPlayTimer()
+	timer.performWithDelay( 5000, updateTimer, 0 )
+end
+
+-------------------------------------------------------------------------------
+-- Collision Detection
+-------------------------------------------------------------------------------
 function onCollision(event)	
 	if (event.phase == "began") then
-		if (event.object1 == ball and event.object2 == ground) then
-			resetScore()
-		elseif (event.object1 == ground and event.object2 == ball) then
-			resetScore()
+		if (event.x > 0 and event.x < 320) then
+			if (event.object1 == ball and event.object2 == ground) then
+				resetScore()
+			elseif (event.object1 == ground and event.object2 == ball) then
+				resetScore()
+			end
 		end
 	end
 end
 
 function listenToCollisionEvents()
 	Runtime:addEventListener( "collision", onCollision )
+end
+
+-------------------------------------------------------------------------------
+-- Logging
+-------------------------------------------------------------------------------
+debugMode = false
+
+debugButton = widget.newButton
+{
+	id = "debug",
+	isEnabled = false,
+	fontSize = 12,
+	font = "Arial",
+	x = 240,
+	y = 0,
+	textOnly = true,
+	labelColor = { default={ 1, 1, 0 } }
+}
+
+function debugText(text)
+	if (debugMode) then
+		debugButton:setLabel(text)
+	end
+end
+
+function logText(text)
+	debugButton:setLabel(text)
 end
 
 
